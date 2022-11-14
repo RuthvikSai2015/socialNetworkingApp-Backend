afda8cb4fcec7921842d144643fe400ed0b03291
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-11-14 09:02:19 +0530

    back-end

--- spec/articles.spec.js
+++ spec/articles.spec.js
@@ -4,9 +4,9 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 const url = path => `http://localhost:3000${path}`;
-describe('Validate Article functionality', () => {
+describe('Validate Article functionalities', () => {
 
-    it('should get current user articles', (done) => {
+    it('should get current user related articles', (done) => {
         fetch(url('/articles'), {
             method: 'GET',
             headers: {'Content-Type': 'application/json'},
@@ -21,7 +21,7 @@ describe('Validate Article functionality', () => {
             })
 
     })
-    it('should add an articles to the current user articles', (done) => {
+    it('should add an articles to the existing current user articles', (done) => {
         let temp = {text: "This is a test article"}
         fetch(url('/article'), {
             method: 'POST',
@@ -48,60 +48,3 @@ describe('Validate Article functionality', () => {
     })
 
 })
-
-
-
-
-
-        // fetch(url('/articles'), {
-        //     method: 'GET',
-        // })
-        //     .then(res =>
-        //         {expect(res.articles.length).toEqual(200)
-        //         })
-        //     .then(res => {
-        //     num = res.articles.length;
-        //     done();
-        // })
-    // })
-    // it ('should add a new article for the logged in user', (done) => {
-    //     // fetch(url('/'))
-    // })
-    // it('should give me current user articles', (done) => {
-    //     fetch(url('/articles'), {
-    //         method: 'GET',
-    //         headers: { 'Content-Type': 'application/json' },
-    //     }).then(res => res.json()).then(res => {
-    //        expect(res.status).toEqual(200);
-    //     }).then(res => {
-    //         expect(num).toEqual(res.articles.length - 1);
-    //         done();
-    //     })
-    // });
-//
-//     it('should add new article with successive article id, return list of articles with new article', (done) => {
-//         // add a new article
-//         // verify you get the articles back with new article
-//         // verify the id, author, content of the new article
-//         let post = {author: 'Tom', body: 'A new post'};
-//         fetch(url('/article'), {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify(post)
-//         }).then(res => res.json()).then(res => {
-//             if (res instanceof Array) {
-//                //TODO test new article expected id, author, post
-//             }
-//             done(new Error('Not Implemented'));
-//         })
-//     });
-//
-//     it('should return an article with a specified id', (done) => {
-//
-//         //call GET /articles/id with the chosen id
-//         // validate that the correct article is returned
-//         //TODO test article expected id, author, post
-//         fetch(url('/articles/' + 3))
-//         done(new Error('Not Implemented'));
-//     })
-// });
--- spec/profile.spec.js
+++ spec/profile.spec.js
@@ -3,27 +3,12 @@
 //  */
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
-// const {Profiles: Profile} = require("./dbConnect");
 
 const url = path => `http://localhost:3000${path}`;
-// new Profile({
-//     username: username,
-//     headline: username + 's initial headline',
-//     followers: [],
-//     email: req.body.email,
-//     zipcode: req.body.zipcode,
-//     dob: req.body.dob,
-//     avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/DWLeebron.jpg/220px-DWLeebron.jpg'
-// }).save()
+
 describe('Validate Profile functionality', () => {
-    let regUser = {username: 'mrj3', password: '1234'};
-    // beforeEach((done) => {
-    //     fetch(url('/login'), {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify(regUser)
-    //     }).then(temp => temp.json()).then((res) =>{cookie = 'sid=' + res['sid']; done();})
-    // })
+    let regUser = {username: 'sara123', password: '1234'};
+  
     it('should get correct headline', (done) => {
 
         fetch(url('/headline/test'),{
@@ -43,7 +28,6 @@ describe('Validate Profile functionality', () => {
             headers: {'Content-Type': 'application/json' },
             body: JSON.stringify({"headline": newHeadline})
         }).then(res => res.json()).then(res => {
-            // expect(res.status).to.equal(200);
             expect(res.headline).toEqual(newHeadline);
             done();
         })
